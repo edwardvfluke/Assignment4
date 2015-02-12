c41a352e6b620f58294e3d2ad97ad64ac5e03b91
Edward Valencia <edward.valencia@fluke.com>
2015-02-12 09:44:13 -0800

    Initial commit

---
+++ src/com/company/Book.java
@@ -0,0 +1,53 @@
+package com.company;
+
+public class Book {
+
+  String title;
+  boolean borrowed;
+
+  // Creates new book
+  public Book (String bookTitle) {
+    title = bookTitle;
+  }
+
+  public void borrowed () {
+    borrowed = true;
+  }
+
+  // Marks the book as not rented
+  public void returned () {
+    // Implement this method
+    borrowed = false;
+  }
+
+  // Returns true if the book is rented, false otherwise
+  public boolean isBorrowed () {
+    // Implement this method
+    if (borrowed) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+
+  // Returns the title of the book
+  public String getTitle () {
+    return title;
+  }
+
+  public void rented () {
+    borrowed = true;
+  }
+
+  public static void main (String[] arguments) {
+    // Small test of the Book class
+    Book example = new Book ("The Da Vinci Code");
+    System.out.println ("Title (should be The Da Vinci Code): " + example.getTitle ());
+    System.out.println ("Borrowed? (should be false): " + example.isBorrowed ());
+    example.rented ();
+    System.out.println ("Borrowed? (should be true): " + example.isBorrowed ());
+    example.returned ();
+    System.out.println ("Borrowed? (should be false): " + example.isBorrowed ());
+  }
+}
